b43c2bb0639df001ff99bb7d1950e6189655a83b
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-02-20 19:04:50 -0500

    added all people objects

---
+++ people.js
@@ -0,0 +1,79 @@
+
+
+let Haven = {
+    name: "Haven",
+    isAdult: false,
+    household: "1329"
+}
+
+let Nova = {
+    name: "Nova",
+    isAdult: false,
+    household: "1329"
+}
+
+let Jennifer = {
+    name: "Jennifer",
+    isAdult: true,
+    household: "1329"
+}
+
+let Joseph = {
+    name: "Joseph",
+    isAdult:true,
+    household: "1329"
+}
+
+let Badi = {
+    name: "Badi",
+    isAdult: false,
+    household: "1337"
+}
+
+let Zayn = {
+    name: "Zayn",
+    isAdult: false,
+    household: "1337"
+}
+
+let Nabil = {
+    name: "Nabil",
+    isAdult: true,
+    household: "1337"
+}
+
+let Jasmine = {
+    name: "Jasmine",
+    isAdult: true,
+    household: "1337"
+}
+
+let Enoch = {
+    name: "Enoch",
+    isAdult: false, 
+    household: "1345"
+}
+
+let Layli = {
+    name: "Layli",
+    isAdult: false, 
+    household: "1345"
+}
+
+let Richard = {
+    name: "Richard",
+    isAdult: true, 
+    household: "1345"
+}
+
+let Carmel = {
+    name: "Carmel",
+    isAdult: true, 
+    household: "1345"
+} 
+
+let Emily = {
+    name: "Emily",
+    isAdult: true, 
+    household: "NC"
+}
