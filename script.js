a538d96bb22ac688faa67061baa53462b9d6d1d9
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-02-20 19:43:18 -0500

    progress

---
+++ script.js
@@ -0,0 +1,95 @@
+
+let Haven = {
+    name: "Haven",
+    isAdult: false,
+    household: "1329",
+    isPaired: false
+}
+
+let Nova = {
+    name: "Nova",
+    isAdult: false,
+    household: "1329",
+    isPaired: false
+}
+
+let Jennifer = {
+    name: "Jennifer",
+    isAdult: true,
+    household: "1329",
+    isPaired: false
+}
+
+let Joseph = {
+    name: "Joseph",
+    isAdult:true,
+    household: "1329",
+    isPaired: false
+}
+
+let Badi = {
+    name: "Badi",
+    isAdult: false,
+    household: "1337",
+    isPaired: false
+}
+
+let Zayn = {
+    name: "Zayn",
+    isAdult: false,
+    household: "1337",
+    isPaired: false
+}
+
+let Nabil = {
+    name: "Nabil",
+    isAdult: true,
+    household: "1337",
+    isPaired: false
+}
+
+let Jasmine = {
+    name: "Jasmine",
+    isAdult: true,
+    household: "1337",
+    isPaired: false
+}
+
+let Enoch = {
+    name: "Enoch",
+    isAdult: false, 
+    household: "1345",
+    isPaired: false
+}
+
+let Layli = {
+    name: "Layli",
+    isAdult: false, 
+    household: "1345",
+    isPaired: false
+}
+
+let Richard = {
+    name: "Richard",
+    isAdult: true, 
+    household: "1345",
+    isPaired: false
+}
+
+let Carmel = {
+    name: "Carmel",
+    isAdult: true, 
+    household: "1345",
+    isPaired: false
+} 
+
+let Emily = {
+    name: "Emily",
+    isAdult: true, 
+    household: "NC",
+    isPaired: false
+}
+
+
+const adults = [Jennifer, Joseph, Jasmine, Nabil, Richard, Carmel, Emily]
+const kids = [Haven, Nova, Enoch, Layli, Badi, Zayn]
