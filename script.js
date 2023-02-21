7166a9b935aad107273044cb18c2658fa3261b71
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-02-21 02:37:36 -0500

    made into unordered list

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ let people = [
     {
         name: "Emily",
         age: 28, 
-        household: "NC",
+        household: "Chapel Hill",
         isPaired: false
     },
     
@@ -200,7 +200,7 @@ function shuffleArray(array) {
     const pairs = adultPairs.concat(childPairs);
 
     pairs.forEach(pair => {
-        document.getElementById("results-container").innerHTML += `${pair.giver.name} will give a gift to <span class="covered">${pair.recipient.name}</span> 🎁 `;
+        document.getElementById("results-container").innerHTML += `<li>${pair.giver.name} will give a gift to <span class="covered">${pair.recipient.name}</span> 🎁 `;
       });
   
     return pairs;
